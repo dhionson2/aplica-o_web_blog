5fb8008197a4c08ba04562fda8248fbcdfb8ee18
DHIONSON PLASTER MOREIRA <dhionsonmoreira@hotmail.com>
2022-03-22 20:51:27 -0300

    Iniciando o projeto

---
+++ codebblog/codebblog/src/main/java/com/spring/codebblog/CodebblogApplication.java
@@ -0,0 +1,13 @@
+package com.spring.codebblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebblogApplication.class, args);
+	}
+
+}
---
+++ codebblog/codebblog/src/test/java/com/spring/codebblog/CodebblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.codebblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
